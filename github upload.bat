d008865b793e263232b0c120e06d86b69d607cf6
ffirsttname <ffirsttname@gmail.com>
2021-08-25 14:05:58 +0800

    25/08/2021 週三 14:05:58.11

---
+++ github upload.bat
@@ -0,0 +1,19 @@
+@echo off
+echo "DOCS PUSH BAT"
+
+echo "1. Move to working directory"
+cd C:\Users\hi\Desktop\notebook_table_data
+
+echo "2. Start submitting code to the local repository"
+git add *
+
+echo "3. Commit the changes to the local repository"
+set now=%date% %time%
+echo "Time:" %now%
+git commit -m "%now%"
+
+echo "4. Push the changes to the remote git server"
+git pull && git push
+
+echo "Batch execution complete!"
+pause
